c294b2717603e55c67114a26fe0eaf046864faef
NJ078078 <Narendra.Jha@cerner.com>
2022-04-11 09:12:11 +0530

    Create new cart and fetch cart

--- Gemfile
+++ Gemfile
@@ -12,3 +12,5 @@ gem "rspec", "~> 3.0"
 gem "rubocop", "~> 0.80"
 
 gem "httparty", "~> 0.20.0"
+
+gem "byebug"
--- lib/vendo_api_sdk_task.rb
+++ lib/vendo_api_sdk_task.rb
@@ -2,65 +2,93 @@
 
 require_relative "vendo_api_sdk_task/version"
 require_relative "vendo_lib/rest_lib"
+require 'byebug'
 
 module VendoApiSdkTask
-
-  ENDPOINTS = 
-  {
-    "authentication": "/spree_oauth/token",
-    "cart": "/storefront/cart"
-  }
-
-  BASE_URL = "https://demo.getvendo.com/api/v2"
-
-  HEADER = {
-    "Accept": "application/json",
-    "Content-Type": "application/json"
-  }
-
-  # authentication
-  def self.authenticate(user_name="", password="")
-    rest_lib = Restlib.new(header: set_header, body: {}, url: form_url("authentication"))
-    response = rest_lib.post
-  end
-
-  # create new cart
-  def self.create_new_cart
-    rest_lib = Restlib.new(header: set_headers, body: {}, url: form_url("cart"))
-    response = rest_lib.post
-  end
-
-  # fetch a cart
-  def self.fetch_cart
-    @api = Restlib.new()
-  end
-
-  # adding item to cart
-  def self.add_to_cart(product_ids = [])
-    @api = Restlib.new()
-  end
-
-  # chaging quantity
-  def self.change_product_quantity(product_id, product_quantity)
-    @api = Restlib.new()
-  end
-  # removing item
-  def self.remove_item(product_id)
-    @api = Restlib.new()
-  end
-  # applying coupon code
-  def self.discount_coupon(coupon_code = "")
-    @api = Restlib.new()
-  end
-
-  def self.set_headers
+  class VendoApi
+    ENDPOINTS = 
     {
-        'Accept' => HEADER["Accept".to_sym],
-        'Content-Type' => HEADER["Content-Type".to_sym]
+      "authentication": "/spree_oauth/token",
+      "cart": "/storefront/cart"
     }
-  end
 
-  def self.form_url(request_type)
-    "#{BASE_URL}#{ENDPOINTS[request_type.to_sym]}"
+    BASE_URL = 
+    {
+      "others": "https://demo.getvendo.com/api/v2",
+      "authentication": "https://demo.getvendo.com/spree_oauth/token"
+    }
+
+    HEADER = {
+      "Accept": "application/json",
+      "Content-Type": "application/json"
+    }
+
+    SUCCESS_CODES = [200, 201]
+
+    # authentication
+    def authenticate(user_name="", password="")
+      rest_lib = Restlib.new(header: set_header, body: {}, url: form_url("authentication", "authentication"))
+      response = rest_lib.post
+    end
+
+    # create new cart
+    def create_new_cart
+      rest_lib = Restlib.new(header: set_headers, body: {}, url: form_url("cart"))
+      response = rest_lib.post
+      return if response.body.nil?
+
+      return unless SUCCESS_CODES.include? response.code
+
+      response.parsed_response["data"]["attributes"]["token"]
+    end
+
+    # fetch a cart
+    def fetch_cart(vendo_cart_token)
+      return if vendo_cart_token.nil?
+
+      header = set_headers.merge!({ "X-Vendo-Order-Token": vendo_cart_token })
+      rest_lib = Restlib.new(header: set_headers, body: {}, url: form_url("cart"))
+      response = rest_lib.get
+      return if response.body.nil?
+
+      return unless SUCCESS_CODES.include? response.code
+
+      @order_cart = response.parsed_response["data"]
+    end
+
+    # adding item to cart
+    def add_to_cart(product_ids = [])
+    end
+
+    # chaging quantity
+    def change_product_quantity(product_id, product_quantity)
+    end
+    
+    # removing item
+    def remove_item(product_id)
+    end
+
+    # applying coupon code
+    def discount_coupon(coupon_code = "")
+    end
+
+    def set_headers
+      {
+          'Accept' => HEADER["Accept".to_sym],
+          'Content-Type' => HEADER["Content-Type".to_sym]
+      }
+    end
+
+    # set_order_token_header
+    def set_order_token_header
+      {
+        "X-Vendo-Order-Token": @vendo_order_token
+      }
+    end
+
+    # form url from base url and endpoint
+    def form_url(request_type, base_url = "others")
+      "#{BASE_URL[base_url.to_sym]}#{ENDPOINTS[request_type.to_sym]}"
+    end
   end
 end
